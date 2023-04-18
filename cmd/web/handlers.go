147375235d5688ab2e96a06c7cec2b794900c0a7
vladislav.tabala <vladislav.tabala@x2sy.com>
2023-04-18 21:33:31 +0300

    init

---
+++ cmd/web/handlers.go
@@ -0,0 +1,92 @@
+package main
+
+import "net/http"
+
+func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "home.page.gohtml", nil)
+}
+
+func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.gohtml", nil)
+}
+
+func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+	_ = app.Session.RenewToken(r.Context())
+
+	// parse form post
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// get email and password from form post
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	user, err := app.Models.User.GetByEmail(email)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// check password
+	validPassword, err := user.PasswordMatches(password)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if !validPassword{
+		msg := Message{
+			To: email,
+			Subject: "Failed log in attempt",
+			Data: "Invalid login attempt!",
+		}
+
+		app.sendEmail(msg)
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// okay, so log user in
+	app.Session.Put(r.Context(), "userID", user.ID)
+	app.Session.Put(r.Context(), "user", user)
+
+	app.Session.Put(r.Context(), "flash", "Successful login!")
+
+	// redirect the user
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
+	// clean up session
+	_ = app.Session.Destroy(r.Context())
+	_ = app.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "register.page.gohtml", nil)
+}
+
+func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+	// create a user
+
+	// send an activation email
+
+	// subscbribe the user to an account
+}
+
+func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+	// validate url 
+
+	// generate an invoice
+
+	// send an email with attachments
+
+	// send an email with the invoice attached
+}
---
+++ go.mod
@@ -0,0 +1,30 @@
+module final-project
+
+go 1.18
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/alexedwards/scs/redisstore v0.0.0-20221206171621-0f0849773278 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.13.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
+	github.com/vanng822/css v1.0.1 // indirect
+	github.com/vanng822/go-premailer v1.20.1 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.13.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
